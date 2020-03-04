b88e345b6ce457e07d001f737c67cfa65ca75f10
canary <mchemweno27@gmail.com>
2020-03-04 23:30:22 +0300

    heroku

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -182,6 +182,7 @@ AWS_STORAGE_BUCKET_NAME = "professionals-backend"
 # AWS Config
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
+S3DIRECT_REGION = 'eu-west-2'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 #STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
