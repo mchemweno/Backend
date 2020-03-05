3092c01ea30fa106c1a008a83fc18fc7eb6f579d
canary <mchemweno27@gmail.com>
2020-03-05 18:39:01 +0300

    heroku and aws s3 working.

--- mpesa_api/views.py
+++ mpesa_api/views.py
@@ -11,4 +11,28 @@ from .models import LipaNaMpesaOnline
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def LipaNaMpesaCallBackURLView(request):
+    """
+    mpesa response format
+    {'Body':
+        {'stkCallback':
+            {'MerchantRequestID': '13852-17960109-1',
+            'CheckoutRequestID': 'ws_CO_050320201800153964',
+            'ResultCode': 0,
+            'ResultDesc': 'The service request is processed successfully.',
+            'CallbackMetadata':
+                        {'Item': [
+                                {'Name': 'Amount', 'Value': 5.0},
+                                {'Name': 'MpesaReceiptNumber', 'Value': 'OC54JS5822'},
+                                {'Name': 'TransactionDate', 'Value': 20200305180032},
+                                {'Name': 'PhoneNumber', 'Value': 254726990002}]}
+                        }
+            }
+        }
+    }
+
+    """
     print(request.data)
+
+    merchant_request_id = request.data['Body']['stkCallback']['MerchantRequestID']
+    print(merchant_request_id, "this should be merachant request id")
+
