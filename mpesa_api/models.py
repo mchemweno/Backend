73780a0ffa50dae8e80fa4229f38ee76d6dc0f2a
canary <mchemweno27@gmail.com>
2020-03-05 19:44:15 +0300

    heroku and aws s3 working.

--- mpesa_api/models.py
+++ mpesa_api/models.py
@@ -1,6 +1,15 @@
+from datetime import datetime
+
 from django.db import models
 
 
 # Create your models here.
 class LipaNaMpesaOnline(models.Model):
-    pass
+    merchant_request_id = models.CharField(max_length=70, default='h')
+    checkout_request_id = models.CharField(max_length=50, default='h')
+    result_code = models.IntegerField(default=0)
+    result_description = models.CharField(max_length=120, default='h')
+    amount = models.FloatField(default=0.0)
+    mpesa_receipt_number = models.CharField(max_length=30, default='h')
+    mpesa_transaction_date = models.DateTimeField(default=datetime.now())
+    phone_number = models.CharField(max_length=15, default='h')
