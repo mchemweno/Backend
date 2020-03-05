0caf6c47a5cace005f873e43888cc296400ee3f3
canary <mchemweno27@gmail.com>
2020-03-05 22:31:46 +0300

    heroku and aws s3 working.

--- professionals_app/views.py
+++ professionals_app/views.py
@@ -159,7 +159,7 @@ def report(request, *args, **kwargs):
         complain_against = data['complain_against']
         subject = f'Complaint against {complain_against}'
         message = f' User by the name {complainant_fname} {complainant_lname} has raised a complaint against user of id {complain_against}.\n Complainant email is {complainant_email}'
-        send_mail(subject, message, os.environ.get('email'))
+        send_mail(subject, message, os.environ.get('email'), [os.environ.get('email')])
         return Response(status=status.HTTP_201_CREATED)
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
