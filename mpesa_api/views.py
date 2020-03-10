c57ffe340553823eae04b5c1abc3929728d5d54c
canary <mchemweno27@gmail.com>
2020-03-10 09:48:18 +0300

    heroku and aws s3 working.

--- mpesa_api/views.py
+++ mpesa_api/views.py
@@ -2,9 +2,9 @@ from datetime import datetime, timezone
 
 import pytz
 from django.http import JsonResponse
-from requests import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 from .serializers import *
 from .models import LipaNaMpesaOnline
